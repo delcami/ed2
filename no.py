9feb309a40e97c29b858ef47c2148411a9ca957f
Camila <camila8gomes@gmail.com>
2026-02-11 15:39:32 -0300

    Update no.py

--- no.py
+++ no.py
@@ -1,3 +1,4 @@
+# Nomes: Camila Gomes Barbosa e Pedro Lucas Carvalho Mendes
 class NO:
     def __init__(self, palavra, linha):
         self.palavra = palavra
@@ -7,7 +8,6 @@ class NO:
         self.dir = None
 
     def contar_elementos(self, no):
-        """Retorna a contagem total de nós em uma subárvore."""
         if no is None:
             return 0
         return 1 + self.contar_elementos(no.esq) + self.contar_elementos(no.dir)
